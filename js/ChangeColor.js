47a005c6052896b41cf6d9f1270be65043b82de5
Rishi Raj Gujadhur <r.r.gujadhur@gmail.com>
2016-11-09 21:55:21 +0400

    Update ChangeColor.js

--- js/ChangeColor.js
+++ js/ChangeColor.js
@@ -1,17 +1,14 @@
 function Happy()
 {
-      
-       document.body.style.backgroundColor = "red";
+       document.body.style.backgroundImage = "url('https://66.media.tumblr.com/cc5f8bbe0654c4dce757cfffa4a28f27/tumblr_n7vtg4lVaN1qbv46ko1_500.gif')";
 }
 
 function Calm()
 {
-   
-     document.body.style.backgroundColor = "blue";
+      document.body.style.backgroundImage = "url('https://media.giphy.com/media/26BRGoqbUQvk8nwTC/giphy.gif')";
 }
-
 
 function Sad()
 {
-      document.body.style.backgroundColor = "yellow";
+      document.body.style.backgroundImage = "url('http://31.media.tumblr.com/d5383c4345000c565d406d280614ed39/tumblr_n6i1p0QzVs1sgk0tao1_500.gif')";
 }
