049f9e8a0ebffbf111fa5af45492e52a1764b1a4
Rishi Raj Gujadhur <r.r.gujadhur@gmail.com>
2016-11-09 18:22:05 +0400

    Add files via upload

---
+++ js/ChangeVideo.js
@@ -0,0 +1,26 @@
+﻿function Happy()
+{
+    $('#video').YTPlayer({
+        fitToBackground: true,
+        videoId: 'ovxnSmGaVlY'
+    });
+}
+
+function Calm()
+{
+    $('#video').YTPlayer({
+        fitToBackground: true,
+        videoId: 'a0Qk-tsGY5Q'
+    });
+}
+
+
+function Sad()
+{
+    $('#video').YTPlayer({
+        fitToBackground: true,
+        videoId: 'XxaC3V78ads'
+    });
+}
+
+
